c20a37c481b9ec0e6f6b94ce57074a43b2cdfd30
Jhon Dayro Rodriguez <jodaromu@gmail.com>
2019-10-01 09:26:46 -0500

    pagina web el punto del cuy 1

---
+++ js/web.js
@@ -0,0 +1,32 @@
+function inicio() {
+
+    window.location = "index.html"
+    
+}
+function ubicacion() {
+
+    window.location = "ubicacion.html"
+
+}
+function plato() {
+
+    window.location = "platos.html"
+
+}
+function informacion() {
+
+    window.location = "informacion.html"
+
+}
+
+function iniciars() {
+
+    window.location = "ingresar.html"
+
+}
+
+function registars() {
+
+    window.location = "registrar.html"
+
+}
